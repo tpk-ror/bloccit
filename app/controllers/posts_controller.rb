95e888743173d0fae18772388deede84adbbaef2
tpk-ror <tomkarels@gmail.com>
2014-04-13 16:02:21 -0500

    figured out what the issue was with editing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,13 +8,12 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-
 def new
   @post = Post.new
 end
-
 def create
   @post = Post.new(params.require(:post).permit(:title, :body))
+  raise # this will short-circuit the method
   if @post.save
     flash[:notice] = "Post was saved."
     redirect_to @post
@@ -24,6 +23,18 @@ def create
   end
 end
 
-def edit
-end
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,3 @@
 class Post < ActiveRecord::Base
   has_many :comments
-
-   def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    if @post.update_attributes(params.require(:post).permit(:title, :body))
-      flash[:notice] = "Post was updated."
-      redirect_to @post
-    else
-      flash[:error] = "There was an error saving the post. Please try again."
-      render :edit
-    end
-  end
-  
 end
