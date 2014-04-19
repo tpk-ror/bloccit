4b0ceeaf4dab5b289e693d872ee3cee07083b760
tpk-ror <tomkarels@gmail.com>
2014-04-19 12:40:30 -0500

    configured app for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,6 @@ Bloccit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+# http://bloccit.herokuapp.com
+  config.action_mailer.default_url_options = { host: 'tk-bloccit.herokuapp.com' }
 end
