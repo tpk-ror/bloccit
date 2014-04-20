3ba89ea2366dc40e9da3d21e4808517b0296f1fa
tpk-ror <tomkarels@gmail.com>
2014-04-20 06:58:24 -0500

    not working locally for some reason, might be routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# app/helpers/application_helper.rb
+
 module ApplicationHelper
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,12 @@
-# app/models/post.rb
-
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   belongs_to :topic
-  
+
   default_scope { order('created_at DESC') }
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 end
